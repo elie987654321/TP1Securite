be5b071b14d86624b8b1eab9b1dff58c469eca84
Elie Poirier-Clement <1445131@cegeplimoilou.ca>
2025-04-04 11:38:35 -0400

    debut bloqueur d'ip spam

--- src/main/java/CegepLimoilouCourSecurite/TP1/RESTService.java
+++ src/main/java/CegepLimoilouCourSecurite/TP1/RESTService.java
@@ -5,6 +5,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.LocalDate;
+import java.util.Scanner;
+
 @RestController
 public class RESTService
 {
@@ -14,7 +24,53 @@ public class RESTService
     @GetMapping("/YouCantDOSMe")
     public void YouCantDOSMe()
     {
-        String ip = request.getRemoteHost();
-        System.out.println(ip);
+        try
+        {
+            String ip = request.getRemoteAddr();
+            String ipConcat = ip.replace(":", "_");
+            String ipFile = "./users_ip_file/" + ipConcat + ".txt";
+            File fileUser = new File(ipFile);
+
+            if(fileUser.exists() && !fileUser.isDirectory())
+            {
+                FileReader reader = new FileReader(ipFile);
+                Scanner scanner = new Scanner(reader);
+                String date = scanner.nextLine();
+
+                if(date.equals(LocalDate.now().toString()))
+                {
+                    String nombreRequetesString = scanner.nextLine();
+
+                    int nombreRequetes = Integer.parseInt(nombreRequetesString);
+
+                    if (nombreRequetes < 10) {
+                        nombreRequetes++;
+                        FileWriter writer = new FileWriter(ipFile);
+                        writer.write(LocalDate.now().toString() + "\n");
+
+                        String nouveauNombreRequeteString = String.valueOf(nombreRequetes);
+                        writer.write(nouveauNombreRequeteString);
+
+
+                        writer.close();
+                    }
+
+                    int t = 0;
+                }
+            }
+            else
+            {
+                fileUser.createNewFile();
+
+                FileWriter writer = new FileWriter(ipFile);
+                writer.write(LocalDate.now().toString() + "\n");
+                writer.write("1");
+                writer.close();
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
     }
 }
